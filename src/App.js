6f3ab36ef2944e47a751fd015ee2ae632517f7c9
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-17 12:55:07 +0600

    add a joining form and succsfully send data on the server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 
 import './App.css';
 import io from 'socket.io-client';
+import { useState } from 'react';
 const socket=io.connect('http://localhost:5000');
 
 function App() {
+  const [userName,setUserName]=useState("")
+  const [roomId,setRoomId]=useState("")
+  const handleJoinARoom= ()=>{
+      if(userName !== "" && roomId !== ""){
+        socket.emit("join_room",roomId);
+        }
+      }
+  
   return (
     <div className="App">
-       live chat application
+        <input 
+         type="text"
+         placeholder='Name...' 
+         onChange={(event)=>setUserName(event.target.value)}
+         />
+        <input 
+         type="text"
+         placeholder='Room Id...' 
+         onChange={(event)=>setRoomId(event.target.value)}
+         />
+         <button onClick={handleJoinARoom}> Join A Room </button>
     </div>
   );
 }
