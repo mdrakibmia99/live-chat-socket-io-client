7baa5783fe7d0791a126f5d7f8198c7ce28a9c72
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-07-28 17:09:09 +0600

    server change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import io from 'socket.io-client';
 import { useState } from 'react';
 import Chat from './Chat';
-const socket=io.connect('https://live-chat-socket-io-server-rakib.vercel.app/');
+const socket=io.connect('http://localhost:5000/');
 
 function App() {
   const [userName,setUserName]=useState("")
